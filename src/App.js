30b0f58991825f5e67eda05977283d3e7ff86140
Lexfer <lexferramirez@gmail.com>
2022-01-04 19:40:44 -0400

    update app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App" style={{ backgroundColor: 'red', color: 'white' }}>
       rama de desarrolloooooo: {process.env.REACT_APP_ENV}
       prueba de klexfer
+      <p>{process.env.REACT_APP_COMPANIA}</p>
     </div>
   );
 }

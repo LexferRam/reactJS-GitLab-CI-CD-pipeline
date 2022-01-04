86a3e65223cfe548be1563618fb224e0b553f847
Lexfer <lexferramirez@gmail.com>
2022-01-04 17:53:33 -0400

    updeate yml

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import React from 'react'
 function App() {
   return (
     <div className="App">
-      rama de produccion:: {process.env.REACT_APP_ENV}
+      rama de produccion::
+      <p>{process.env.REACT_APP_ENV}</p>
+      <p>{process.env.REACT_APP_COMPANIA}</p>
       lexfer ramirez
     </div>
   );
